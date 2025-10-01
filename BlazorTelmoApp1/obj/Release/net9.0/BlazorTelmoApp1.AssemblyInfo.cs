42af128ac862b111fa4956cb117384e9318427df
TelmoFrank <frank.eagle1@gmail.com>
2025-10-02 06:27:06 +1000

    staging playing

--- BlazorTelmoApp1/obj/Release/net9.0/BlazorTelmoApp1.AssemblyInfo.cs
+++ BlazorTelmoApp1/obj/Release/net9.0/BlazorTelmoApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTelmoApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c231946bfefd2820668d1b81b2edb8bd6639a779")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTelmoApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTelmoApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
